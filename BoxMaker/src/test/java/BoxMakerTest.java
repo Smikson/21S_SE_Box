947ae0d6366312afeffa4ad5f08f970d8cd2c4dc
Wyatt Witemeyer <43794205+Smikson@users.noreply.github.com>
2021-03-03 17:28:22 -0600

    Add files via upload
    Changes made include the two additional tests from class on 3/3, and the tests now use the getSVG() function from BoxMaker

--- BoxMaker/src/test/java/BoxMakerTest.java
+++ BoxMaker/src/test/java/BoxMakerTest.java
@@ -33,38 +33,73 @@ public class BoxMakerTest {
         new ArrayList<Object>().get(0);
     }
 
+    // Tests to check that the String contains our strict paths
     @Test
     public void test_03_inString() {
-    	assertTrue(bm.theString.contains("<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\""));
+    	assertTrue(bm.getSVG().contains("<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\""));
     }
 
     @Test
     public void test_04_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 40.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 40.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
     }
 	
 	@Test
     public void test_05_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 65.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 65.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
     }
 	
 	@Test
     public void test_06_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 90.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 90.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
     }
 	
 	@Test
     public void test_07_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 40.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 40.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
     }
 	
 	@Test
     public void test_08_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 65.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 65.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
     }
 	
 	@Test
     public void test_09_inString() {
-    	assertTrue(bm.theString.contains("path d=\"M 90.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    	assertTrue(bm.getSVG().contains("path d=\"M 90.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\""));
+    }
+
+    // Class Extra Unit Tests
+    @Test
+    public void test_10_unitTest() {
+    	// Make sure we're moving somwhere in each path
+    	String lines[];
+    	lines = bm.getSVG().split("\n");
+    	boolean passed = true;
+    	for (int i = 0; i < lines.length; i++) {
+    		if (lines[i].contains("path d=")) {
+    			// If we have a path, and we don't move, fail the test
+    			if (!(lines[i].contains("M") || lines[i].contains("m"))) {
+    				passed = false;
+    			}
+    		}
+    	}
+    	// See if the boolean stayed true
+    	assertTrue(passed);
+    }
+
+    @Test
+    public void test_11_unitTest() {
+    	// Test if we are drawing 6 total sides, 6 paths
+    	String lines[];
+    	lines = bm.getSVG().split("\n");
+    	int pathCount = 0;
+    	for (int i = 0; i < lines.length; i++) {
+    		if (lines[i].contains("path d=")) {
+    			pathCount++;
+    		}
+    	}
+    	// Make sure we have 6 paths
+    	assertEquals(pathCount, 6);
     }
 }
