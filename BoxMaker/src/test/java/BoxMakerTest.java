159f987742d2ab797418caa800b391665c8b7f45
Wyatt Witemeyer <43794205+Smikson@users.noreply.github.com>
2021-03-25 10:22:16 -0500

    Add files via upload
    Added 5 tests (and edited 5 tests) to thoroughly test the different inputs users could give for the new function prompting for dimensions.

--- BoxMaker/src/test/java/BoxMakerTest.java
+++ BoxMaker/src/test/java/BoxMakerTest.java
@@ -104,44 +104,101 @@ public class BoxMakerTest {
     	assertEquals(pathCount, 6);
     }
 
-    // Five more tests to check the new user input function works as intended
+    // Tests to check the user input function works as intended when using System.in for user input
     @Test
     public void test_12_userInput() {
-    	// Test we return true when the user's response is Y (without printing the prompts)
-    	String response = "Y";
+    	// Test we return the correct dimension from user input (without printing the prompts)
+    	String response = "3.7\n";
     	System.setIn(new ByteArrayInputStream(response.getBytes()));
-    	assertTrue(bm.filePrompt(false));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 3.7, 0.0001); // It should be the same, use 0.0001 as precision check
+    	sc.close();
     }
 
     @Test
     public void test_13_userInput() {
-    	// Test we return false when the user's response is N
-    	String response = "N";
+    	// Test that we are also accurate within the 0.0001 on unneccesarily long inputs
+    	String response = "3.123456789123456789123456789\n";
     	System.setIn(new ByteArrayInputStream(response.getBytes()));
-    	assertTrue(!bm.filePrompt(false));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 3.123456789123456789123456789, 0.0001);
+    	sc.close();
     }
 
     @Test
     public void test_14_userInput() {
-    	// Test we return true when the user's response is garbage for two lines then Y
-    	String response = "gar\nbage\nY";
+    	// Test to make sure we still get the number regardless of white space
+    	String response = "\t 7.5\n";
     	System.setIn(new ByteArrayInputStream(response.getBytes()));
-    	assertTrue(bm.filePrompt(false));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 7.5, 0.0001);
+    	sc.close();
     }
 
     @Test
     public void test_15_userInput() {
-    	// Test we return false when the user's response is garbage for two lines then N
-    	String response = "gar\nbage\nN";
+    	// If the user enters garbage before the number, it should not take it, instead prompting again (getting the second number)
+    	String response = "garbage 3.7\n7.5\n";
     	System.setIn(new ByteArrayInputStream(response.getBytes()));
-    	assertTrue(!bm.filePrompt(false));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 7.5, 0.0001);
+    	sc.close();
     }
 
     @Test
     public void test_16_userInput() {
-    	// Test we return true when the user's response is lowercase y
-    	String response = "y";
+    	// If the user enters garbage after the number, it should not take it, instead prompting again (getting the second number)
+    	String response = "3.7 garbage\n7.5\n";
     	System.setIn(new ByteArrayInputStream(response.getBytes()));
-    	assertTrue(bm.filePrompt(false));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 7.5, 0.0001);
+    	sc.close();
+    }
+
+    // More tests for user input focusing on edge-case functionality
+    @Test
+    public void test_17_userInput() {
+    	// If two numbers are entered on separate lines (even with whitespace, make sure we just get the first
+    	String response = " \t3.7 \t\n \t7.5 \t\n";
+    	System.setIn(new ByteArrayInputStream(response.getBytes()));
+    	Scanner sc = new Scanner(System.in);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 3.7, 0.0001);
+    	sc.close();
+    }
+
+    @Test
+    public void test_18_userInput() {
+    	// If two numbers are entered on the same line, the line is ignored, prompting again
+    	String response = "3.7 7.5\n8.6\n";
+    	Scanner sc = new Scanner(response);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 8.6, 0.0001);
+    	sc.close();
+    }
+
+    @Test
+    public void test_19_userInput() {
+    	// If a number entered is below the range, the line is ignored, prompting again
+    	String response = "2.7\n7.5\n";
+    	Scanner sc = new Scanner(response);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 7.5, 0.0001);
+    	sc.close();
+    }
+
+    @Test
+    public void test_20_userInput() {
+    	// If a number entered is above the range, the line is ignored, prompting again
+    	String response = "11.7\n7.5\n";
+    	Scanner sc = new Scanner(response);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 7.5, 0.0001);
+    	sc.close();
+    }
+
+    @Test
+    public void test_21_userInput() {
+    	// Make sure we will continually prompt until a good value is entered
+    	String response = "2.7\n11.7\ngar\nbage\ngarbage\n-17.9\n9999999.9999999\n3.7\n";
+    	Scanner sc = new Scanner(response);
+    	assertEquals(bm.promptDimension(sc, 3, 10, false), 3.7, 0.0001);
+    	sc.close();
     }
 }
