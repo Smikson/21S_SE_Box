f865a3c9b6bacb4e95bce706abf14540a8c1d436
rosycg <78099742+rosycg@users.noreply.github.com>
2021-04-08 14:39:45 -0500

    Update BoxMaker.java
    Deleted static String and getSVG() which returned the static String.

--- BoxMaker/src/main/java/BoxMaker.java
+++ BoxMaker/src/main/java/BoxMaker.java
@@ -19,32 +19,6 @@ import java.util.*;
 // BoxMaker
 public class BoxMaker {
 
-	public static final String theString = "<?xml version='1.0' encoding='us-ascii'?>\n"
-						+ "<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\" xmlns=\"http://www.w3.org/2000/svg\"\n"
-						+ "xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n"
-						+ "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n"
-						+ "xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n"
-						+ "<g id=\"square1\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 40.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "<g id=\"square2\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 65.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "<g id=\"square3\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 90.0 10.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "<g id=\"square4\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 40.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "<g id=\"square5\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 65.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "<g id=\"square6\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n"
-						+ "<path d=\"M 90.0 40.0 h 20.0 v 20.0 h -20.0 v -20.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n"
-						+ "</g>\n"
-						+ "</svg>";
-
-
 	// Function to return the SVG String that needs to be printed -- NOTE: currently does not account for thinkness of wood
 	public static String getSVG(double length, double width, double height) {
 		// Setup
@@ -81,9 +55,6 @@ public class BoxMaker {
 
 		// Return the resulting String
 		return ret;
-	}
-	public static String getSVG() {
-		return theString;
 	}
 
 	// Function to prompt the user for a numeric (double) value -- used to specify dimensions
