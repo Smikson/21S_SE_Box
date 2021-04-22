13be2da787bb5877dcb9e3b9c3fe461b553f5146
Wyatt Witemeyer <43794205+Smikson@users.noreply.github.com>
2021-04-22 16:31:09 -0500

    Add files via upload
    Added functions to properly prompt for each portion of our A requirement: inserts. We can now prompt Y/N, and for integers, but note this version does nothing except display the information received, it does not actually create any inserts yet.

--- BoxMaker/src/main/java/BoxMaker.java
+++ BoxMaker/src/main/java/BoxMaker.java
@@ -143,12 +143,75 @@ public class BoxMaker {
 		return promptDimension(sc, lowerbound, upperbound, true);
 	}
 
+	// Function to prompt the user for an int value -- used to specify number of rows/cols for inserts
+	public static int promptInteger(Scanner sc, int lowerbound, int upperbound, boolean display) {
+		// Loop until valid response is given, store in "choice" to return
+		int choice;
+		while (true) {
+			// Make sure an int is entered in their reply
+			if (sc.hasNextInt()) {
+				// Get the int and make sure its in the specified bound
+				choice = sc.nextInt();
+				String extra = "";
+				extra = sc.nextLine();
+				if (choice >= lowerbound && choice <= upperbound && extra.trim().equals("")) {
+					break;
+				}
+				// If the double is not in the specified bound, display as such (when not testing)
+				else if (display) {
+					System.out.print("Please only a value within the requested bound: ");
+				}
+			}
+			// If an double was not entered, display as such (when not testing)
+			else {
+				if (display) {
+					System.out.print("Please enter an integer value as a response: ");
+				}
+				sc.nextLine();
+			}
+		}
+		return choice;
+	}
+	// By default display the prompt (only don't when testing)
+	public static int promptInteger(Scanner sc, int lowerbound, int upperbound) {
+		return promptInteger(sc, lowerbound, upperbound, true);
+	}
+
+	// Function to prompt the user if they want box inserts
+	public static boolean promptInserts(Scanner sc, boolean display) {
+		// Loop until Y or N is entered
+		while (true) {
+			// Get the response, upper case it so it doesn't matter if entered as lower or upper
+			if (display) {
+				System.out.print("Response: ");
+			}
+			String response = sc.nextLine().toUpperCase();
+
+			// If yes, return true
+			if (response.equals("Y")) {
+				return true;
+			}
+			// If no, return false
+			else if (response.equals("N")) {
+				return false;
+			}
+			// Otherwise, it is an invalid response, get the prompt again
+			else {
+				if (display) {
+					System.out.println("Invalid response. Please respond with only \"Y\" or \"N\"");
+				}
+			}
+		}
+	}
+	// By default display the prompt (only don't when testing)
+	public static boolean promptInserts(Scanner sc) {
+		return promptInserts(sc, true);
+	}
+
 	// Main method, what is run when the program is run
     public static void main(String[] args) {
     	// Scanner to control user input
     	Scanner sc = new Scanner(System.in);
-
-    	/**************** Future: insert prompt for design selection ******************/
 
     	// Prompt for the length, width, and height dimensions to be used for the box
     	System.out.println("Enter the desired dimensions for your box (in inches), each within 2-5in:");
@@ -159,8 +222,30 @@ public class BoxMaker {
     	System.out.print("Height: ");
     	double height = promptDimension(sc, 2, 5);
 
+    	// Prompt the user for if they want box inserts
+		System.out.println("Would you like to the SVG file written to \"output.svg\"? [Y/N]");
+		boolean inserts = promptInserts(sc);
+
+		// Prompt for the insert height desired
+		System.out.println("What height would you like the inserts to be? 1-"+height+"in:");
+		System.out.print("Insert Height: ");
+		double insertHeight = promptDimension(sc, 1, height);
+
+		// Prompt for the number of rows
+		System.out.println("How many rows of sections do you want? 1-5:");
+		System.out.print("Rows: ");
+		int rows = promptInteger(sc, 1, 5);
+
+		// Prompt for the number of columns
+		System.out.println("How many columns of sections do you want? 1-5:");
+		System.out.print("Columns: ");
+		int cols = promptInteger(sc, 1, 5);
+
     	// Display the chosen dimensions so the user can double check what they specified
     	System.out.println("Generating box with size: " + length + "in x " + width + "in x " + height + "in.");
+    	if (inserts) {
+    		System.out.println("Additionally generating " + insertHeight + "in tall inserts to create " + rows + " x " + cols + " sections in the box.");
+    	}
 
 		// Write the output String to an SVG file (catch any IOExceptions)
 		try {
